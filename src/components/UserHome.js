6f7e74c04392084d8527b2ad7586d02e72264e52
JuanDiDonato <didonatojuan7@gmail.com>
2021-09-30 17:35:31 -0300

    Getting images

--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -3,33 +3,30 @@ import axios from 'axios'
 
 export default function UserHome() {
 
-    const token = "IGQVJXYVJVTHJIcTRDeG5Cc2hMYzYyVEZAtT2J5OXRBWVpxU0YxcGpxN3dsNXV2Ry1CQzV4cmIzYldZAd1VTdGxtaEpJaVRJeWFqYS1Qd1ZAtSDQ1Q1dZAM1h5RVBNVzNZAMlJON0FMZAFR2M2U1WF9uYmF4VAZDZD"
+    const token = "IGQVJWQmNEc01sVEZA3UElDWktHbWQ0UGQ2NXg1SGpKX0pkbVFFck9JM3JCVngzX1F3ZAUd3Y293SnEtU2ZAwN3FFQm94elRrTFdzNDk5Y2NNN1YzUlBBOFRxOWNRODdIMmVWTzM3ZAE5sU3ZArazhoNWo5NQZDZD"
     const [user_data, setUser_data] = useState([])
     
     useEffect(() => {
         const GetUserData = async () => {
-            const {data : {data}} = await axios.get('https://graph.instagram.com/me/media?fields=id,caption&access_token='+token)
+            const {data : {data}} = await axios.get('https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token='+token)
             console.log(data);
             setUser_data(data)
-            
         }
-
         GetUserData()
-        
     }, [])
-
-    const Ids =  () => {
-        user_data.map( async data => {
-            console.log(data.id)
-            const ImagesData = await axios.get('https://graph.instagram.com/'+data.id+'?fields=id,media_type,media_url,username,timestamp&access_token='+token)
-            console.log(ImagesData);
-        })
-    }
     
-    return (
+
+    return(
         <div>
-            <h1>cargando</h1>
-            <button type="submit" onClick={Ids}>obtener</button>
+            {user_data.map(data => {
+                return(
+                    <h2  key={data.id}>{data.media_user}</h2>
+                )
+            })}
         </div>
     )
-}
+        
+
+    }
+
+
