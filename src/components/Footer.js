1ef931ab4a06d84b97e07f259fe5f181c6066ca8
JuanDiDonato <didonatojuan7@gmail.com>
2021-10-01 20:33:33 -0300

    add buttons with css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
             <img src={logo} alt="Lo de pochi Sublimaciones" className="imagesA" />
             <h4>LoDePochi Sublimaciones</h4>
             <div className="contacto">
-            <a href="https://www.instagram.com/lodepochi.sublimaciones/?hl=es-la"><i className="fa fa-instagram mr-1"></i>Instagram</a>
+            <a href="https://www.instagram.com/lodepochi.sublimaciones/?hl=es-la" className="instagram"><i className="fa fa-instagram mr-1"></i>Instagram</a>
             <a href="https://www.facebook.com/profile.php?id=100062124255572"><i className="fa fa-facebook mr-1"></i>Facebook</a>
             <a href="https://wa.me/5492235164764"><i className="fa fa-whatsapp mr-1"></i>Whatsapp</a>
             </div>
--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -2,26 +2,29 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import moment from 'moment'
 import 'moment/locale/es'
-/*LOGOS*/
-import cantimplora from '../assets/img/cantimplora.ico' 
+
 
 export default function UserHome() {
 
     const token = "IGQVJYOThHX3N6dFJsLUhQRzRKeGotdXVteVdMcGpvSnhiTnlJcjM1Vl95VzE4NmxuX21tM0czVm5UMmpIRW1Tc0pOdjNSdGVPZAHRJS0ZA0SGlGNHdOdnpxTkdGaGdMaU5JS2NXLVRZAaHlsTVFBbjBZATwZDZD"
     const [user_data, setUser_data] = useState([])
+
     
     useEffect(() => {
         const GetUserData = async () => {
             const {data : {data}} = await axios.get('https://graph.instagram.com/me/media?fields=id,media_url,media_type,caption,timestamp&access_token='+token)
-            console.log(data);
             setUser_data(data)
         }
         GetUserData()
+        // eslint-disable-next-line
     }, [])
-    
+
+
+
+
 
     return(
-        <div>
+        <div id="body">
             <div className="desc">
                 <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laudantium animi nemo reiciendis incidunt, velit dolores rem minima similique necessitatibus ipsam possimus ullam architecto repudiandae voluptates aut quasi quo. Laborum.</h4>
             </div>
@@ -29,7 +32,7 @@ export default function UserHome() {
                 <h2>Realizamos sublimaciones en </h2>
                 <div  className="cat">
                     <div>Tazas ✓</div>
-                    <div><img src={cantimplora} alt="barbijo" className="cat-logos" /> Cantimploras ✓</div>
+                    <div> Cantimploras ✓</div>
                     <div>Remeras ✓</div>
                     <div>Baby Body ✓</div>
                     <div>Gorras ✓</div>
@@ -39,30 +42,33 @@ export default function UserHome() {
                     <div>¡Y muchos mas! ✓</div>
                 </div>
             </div>
-            <div className="contenidoB">
+            <div className="contenidoB"> 
                 {user_data.map(data => {
-                    console.log(data.media_url);
                     return(
-                        <div key={data.id} >
+                        <div key={data.id}>
                             {data.media_type === 'VIDEO' ?
                                 <div className="card m-3">
                                     <video src={data.media_url} autoPlay loop muted className="imagesB"></video>
-                                    <div className="card-body">{data.caption ?<h5 className="card-text">{data.caption}</h5> : null}</div>
-                                    <h5>{moment(data.timestamp).fromNow()}</h5> 
+                                    <div className="card-body">
+                                        {data.caption ? <h5 className="card-text">{data.caption}</h5> : null}
+                                        <h5>{moment(data.timestamp).fromNow()}</h5> 
+                                        <a href={data.media_url} className="ver" >Ver</a>
+                                        </div>
                                 </div>
-                             
                             : 
                             <div className="card m-3">
                                 <img src={data.media_url} className="card-img-top imagesB" alt={data.id}/>
                                 <div className="card-body">
                                     {data.caption ? <h5 className="card-text">{data.caption}</h5> : null}
                                     <h5>{moment(data.timestamp).fromNow()}</h5> 
-                                   
-                                    
+                                    <a href={data.media_url} className="ver" >Ver</a>
                                 </div> 
-                            </div>
+                                
+                            </div>   
                             } 
+                            
                         </div>
+                        
                     )
                 })}
             </div>
