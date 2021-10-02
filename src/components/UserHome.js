fe88208899d30f2b2df6f777625a4f5627740b95
franmedi99 <franmedi_mdp@hotmail.com>
2021-10-02 12:05:33 -0300

    adding buscador

--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -9,19 +9,48 @@ export default function UserHome() {
 
     const token = "IGQVJYOThHX3N6dFJsLUhQRzRKeGotdXVteVdMcGpvSnhiTnlJcjM1Vl95VzE4NmxuX21tM0czVm5UMmpIRW1Tc0pOdjNSdGVPZAHRJS0ZA0SGlGNHdOdnpxTkdGaGdMaU5JS2NXLVRZAaHlsTVFBbjBZATwZDZD"
     const [user_data, setUser_data] = useState([])
+    const [results, setResults] = useState([])
+    const [query, setQuery] = useState([])
     
     useEffect(() => {
         const GetUserData = async () => {
-            const {data : {data}} = await axios.get('https://graph.instagram.com/me/media?fields=id,media_url,media_type,caption,timestamp&access_token='+token)
-            console.log(data);
+            const {data : {data}} = await axios.get('https://graph.instagram.com/me/media?fields=id,media_url,media_type,caption,timestamp&limit=100&access_token='+token)
             setUser_data(data)
+            setResults(data)
+
+            const arraytosearch = []
+            let count = 0
+            data.forEach(element => {
+                count = count + 1
+                if(element.caption){
+                    arraytosearch.push(element)
+                }
+                if (count === data.length) {
+                  setQuery(arraytosearch)
+                }
+              });
+         
         }
         GetUserData()
     }, [])
+    const buscador = (query,search) => {
+   
+        const FilterResult = query.filter((obj)=>obj.caption.toLowerCase().includes(search.toLowerCase()))
+        if(search.length > 0){
+            setResults(FilterResult)
+        }else{
+            setResults(user_data)
+        }
+    }
+    
+
+    const onChange = e =>{
+        buscador(query,e.target.value)
+      }
     
 
     return(
-        <div>
+        <div >
             <div className="desc">
                 <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laudantium animi nemo reiciendis incidunt, velit dolores rem minima similique necessitatibus ipsam possimus ullam architecto repudiandae voluptates aut quasi quo. Laborum.</h4>
             </div>
@@ -39,9 +68,21 @@ export default function UserHome() {
                     <div>¡Y muchos mas! ✓</div>
                 </div>
             </div>
-            <div className="contenidoB">
-                {user_data.map(data => {
-                    console.log(data.media_url);
+
+
+        <div className="input-group col-md-8 mx-auto mt-5 mb-5 bg-buscador ">
+        <input type="text" name="search" placeholder="Escribí un producto..." className="form-control text-light mb-3 blanco"  onChange={onChange}/> 
+  <div className="input-group-append ">
+      <i className="fa fa-search  fa-2x mt-3 text-light" aria-hidden="true"></i>
+  </div>
+</div>
+
+
+            <div className="contenidoB  mx-auto">
+                {
+      
+                
+                results.map(data => {
                     return(
                         <div key={data.id} >
                             {data.media_type === 'VIDEO' ?
@@ -64,7 +105,11 @@ export default function UserHome() {
                             } 
                         </div>
                     )
-                })}
+                })
+                
+            
+                
+                }
             </div>
         </div>
     )
