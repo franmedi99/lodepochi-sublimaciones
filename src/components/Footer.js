bab90a9282f6d1a14021b7a7864fa240247d5483
JuanDiDonato <didonatojuan7@gmail.com>
2021-10-02 16:06:31 -0300

    finishing project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,11 @@ import logo from '../assets/img/logo2.jpg'
 
 export default function Footer() {
     return (
-        <div className="footer">
+        <div className="footer2 mt-5">
             <img src={logo} alt="Lo de pochi Sublimaciones" className="imagesA" />
             <h4>LoDePochi Sublimaciones</h4>
             <div className="contacto">
-            <a href="https://www.instagram.com/lodepochi.sublimaciones/?hl=es-la" className="instagram"><i className="fa fa-instagram mr-1"></i>Instagram</a>
+            <a href="https://www.instagram.com/lodepochi.sublimaciones/?hl=es-la"><i className="fa fa-instagram mr-1"></i>Instagram</a>
             <a href="https://www.facebook.com/profile.php?id=100062124255572"><i className="fa fa-facebook mr-1"></i>Facebook</a>
             <a href="https://wa.me/5492235164764"><i className="fa fa-whatsapp mr-1"></i>Whatsapp</a>
             </div>
--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -10,7 +10,7 @@ export default function UserHome() {
     const [user_data, setUser_data] = useState([])
     const [results, setResults] = useState([])
     const [query, setQuery] = useState([])
-     // eslint-disable-next-line
+    // eslint-disable-next-line
     const [modaldata,setModalData] =useState({img:"",caption:"",media_type:""})
     
     useEffect(() => {
@@ -18,7 +18,6 @@ export default function UserHome() {
             const {data : {data}} = await axios.get('https://graph.instagram.com/me/media?fields=id,media_url,media_type,caption,timestamp&limit=100&access_token='+token)
             setUser_data(data)
             setResults(data)
-
             const arraytosearch = []
             let count = 0
             data.forEach(element => {
@@ -30,13 +29,12 @@ export default function UserHome() {
                   setQuery(arraytosearch)
                 }
               });
-         
         }
         GetUserData()
         // eslint-disable-next-line
     }, [])
+
     const buscador = (query,search) => {
-   
         const FilterResult = query.filter((obj)=>obj.caption.toLowerCase().includes(search.toLowerCase()))
         if(search.length > 0){
             setResults(FilterResult)
@@ -44,29 +42,29 @@ export default function UserHome() {
             setResults(user_data)
         }
     }
-    console.log(modaldata);
-
     const onChange = e =>{
         buscador(query,e.target.value)
       }
       const vermodal = obj =>{
-        console.log(obj);
        setModalData({img:obj.img,caption:obj.caption,media_type:obj.media_type})
        const vermodal = document.getElementById('vermodal')
        vermodal.click()
       }
 
     return(
-        <div >
-<button type="hidden" data-toggle="modal" data-target=".bd-example-modal-lg" className="ds" id="vermodal" ></button>
-<div className="modal fade bd-example-modal-lg" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div className="modal-dialog modal-xl elwidth">
-    <div className="modal-content mt-5 mb-5">
-    {modaldata.media_type === 'VIDEO' ? <video src={modaldata.img} autoPlay loop muted ></video>: <img src={modaldata.img} className="card-img-top " alt="imagen"/>} 
-    </div>
-  </div>
-</div>
+        <div>
+            <button type="hidden" data-toggle="modal" data-target=".bd-example-modal-lg" className="ds" id="vermodal" ></button>
+            <div className="modal fade bd-example-modal-lg" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-xl">
 
+                    <div className="modal-content mt-5 mb-5 multimedia">
+                        <button type="button" className="close boton-modal  p-1 stext-light" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                        </button>
+                    {modaldata.media_type === 'VIDEO' ? <video src={modaldata.img} autoPlay loop muted ></video>: <img src={modaldata.img} className="card-img-top " alt="imagen"/>} 
+                    </div>
+                </div>
+            </div>
 
             <div className="desc">
                 <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laudantium animi nemo reiciendis incidunt, velit dolores rem minima similique necessitatibus ipsam possimus ullam architecto repudiandae voluptates aut quasi quo. Laborum.</h4>
@@ -87,18 +85,16 @@ export default function UserHome() {
             </div>
 
 
-        <div className="input-group col-md-8 mx-auto mt-5 mb-5 bg-buscador ">
-        <input type="text" name="search" placeholder="Escribí un producto..." className="form-control text-light  mb-3 blanco"  onChange={onChange}/> 
-  <div className="input-group-append ">
-      <i className="fa fa-search  fa-2x mt-3 text-light" aria-hidden="true"></i>
-  </div>
-</div>
+        <div className="input-group mx-auto mt-5 mb-5 bg-buscador ">
+            <input type="text" name="search" placeholder="Escribí un producto..." className="form-control ml-3 mb-3 blanco"  onChange={onChange}/> 
+            <div className="input-group-append ">
+                <i className="fa fa-search fa-2x mt-4 ml-2 mr-1 text-light" aria-hidden="true"></i> 
+            </div>
+        </div>
 
 
-            <div className="contenidoB  mx-auto">
+            <div className="contenidoB">
                 {
-      
-                
                 results.map(data => {
                     return(
                         <div key={data.id}>
@@ -108,7 +104,6 @@ export default function UserHome() {
                                     <div className="card-body">
                                         {data.caption ? <h5 className="card-text">{data.caption}</h5> : null}
                                         <h5>{moment(data.timestamp).fromNow()}</h5> 
-                               
                                         <a href="#/" className="ver"  onClick={(e) => vermodal({img:data.media_url ,caption:data.caption,media_type:data.media_type})} >Ver</a>
                                         </div>
                                 </div>
@@ -118,27 +113,18 @@ export default function UserHome() {
                                 <div className="card-body">
                                     {data.caption ? <h5 className="card-text">{data.caption}</h5> : null}
                                     <h5>{moment(data.timestamp).fromNow()}</h5> 
-                  
-                                    
                                     <a href="#/" className="ver"  onClick={(e) => vermodal({img:data.media_url ,caption:data.caption,media_type:data.media_type})} >Ver</a>
                                 </div> 
-                                
                             </div>   
                             } 
-                            
                         </div>
-                        
                     )
                 })
-                
-            
-                
                 }
             </div>
         </div>
     )
-        
-    
-    }
+
+}
 
 
